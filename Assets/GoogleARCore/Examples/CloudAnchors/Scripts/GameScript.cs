c464f5e50301476bc5d6700011629726ca3e63d2
Victor Miranda <vmm@cin.ufpe.br>
2018-12-11 15:32:55 -0300

    inicio menu trocando telas

---
+++ Assets/GoogleARCore/Examples/CloudAnchors/Scripts/GameScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour {
+
+    int state = 0;
+
+    public Canvas[] telas = new Canvas[5]; 
+
+	// Use this for initialization
+	void Start () {
+        this.telas[0].enabled = true;
+	}
+
+    public void entrarClick()
+    {
+        this.telas[state].enabled = false;
+        this.state += 1;
+        this.telas[state].enabled = true;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
