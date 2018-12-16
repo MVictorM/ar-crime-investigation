3cf38af4db3c920e779621b8a1f4425e16c9f496
Hilton Pintor <hiltonpintor@gmail.com>
2018-12-15 22:30:22 -0300

    Adding Home

---
+++ iOS-Native/investigAR/investigAR/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  investigAR
+//
+//  Created by Hilton Pintor Bezerra Leite on 15/12/18.
+//  Copyright © 2018 Hilton Pintor Bezerra Leite. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+}
+
