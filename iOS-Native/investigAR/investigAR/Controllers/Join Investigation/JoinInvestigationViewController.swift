9874b2de4fefcc6c48eb9ed5090b96074e5917f2
Hilton Pintor <hiltonpintor@gmail.com>
2018-12-17 11:56:17 -0300

    Adding resolve flux

--- iOS-Native/investigAR/investigAR/Controllers/Join Investigation/JoinInvestigationViewController.swift
+++ iOS-Native/investigAR/investigAR/Controllers/Join Investigation/JoinInvestigationViewController.swift
@@ -7,22 +7,65 @@
 //
 
 import UIKit
+import Firebase
 
 class JoinInvestigationViewController: UIViewController {
+    
+    var firebaseReference: DatabaseReference?
+    var anchorID: String?
 
     @IBOutlet weak var roomNumberTextField: UITextField!
     @IBOutlet weak var enterButton: UIButton!
     @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     @IBAction func tapEnterButton(_ sender: Any) {
-        self.searchRoom(number: self.roomNumberTextField.text ?? "")
+        guard let roomNumber = self.roomNumberTextField.text, roomNumber != "" else {
+            self.errorLabel.isHidden = false
+            self.errorLabel.text = "Digite um número válido"
+            return
+        }
+        
+        self.resolveAnchor(roomCode: roomNumber)
     }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         self.roomNumberTextField.becomeFirstResponder()
+        self.firebaseReference = Database.database().reference()
     }
     
-    func searchRoom(number: String) {
-        print("Procurando \(number)")
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let resolveVC = segue.destination as? ResolveViewController {
+            resolveVC.anchorID = self.anchorID!
+        }
+    }
+
+    
+    // MARK: - Resolving Anchor
+    func resolveAnchor(roomCode: String) {
+        self.activityIndicator.startAnimating()
+        
+        weak var weakSelf: JoinInvestigationViewController? = self
+        
+        self.firebaseReference!.child("hotspot_list").child(roomCode)
+            .observe(DataEventType.value) { (snapshot) in
+                DispatchQueue.main.async {
+                    let strongSelf: JoinInvestigationViewController? = weakSelf
+                    
+                    strongSelf?.activityIndicator.stopAnimating()
+                    
+                    if let sanpshotValue = snapshot.value as? [String: Any],
+                        let anchorID = sanpshotValue["hosted_anchor_id"] as? String {
+                        strongSelf?.firebaseReference?.child("hotspot_list").child(roomCode).removeAllObservers()
+                        strongSelf?.anchorID = anchorID
+                        self.performSegue(withIdentifier: "toResolve", sender: nil)
+                    } else {
+                        self.errorLabel.isHidden = false
+                        self.errorLabel.text = "Cena não encontrada"
+                    }
+                }
+        }
     }
 }
